ef6cc241bc9fb423dc672f2502155abda75981f4
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-07-03 14:29:42 -0400

    adds migrations

--- db/migrate/20140703175804_create_listings.rb
+++ db/migrate/20140703175804_create_listings.rb
@@ -1,7 +1,8 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-
+      t.string      :address
+      t.integer     :rooms
       t.timestamps
     end
   end
