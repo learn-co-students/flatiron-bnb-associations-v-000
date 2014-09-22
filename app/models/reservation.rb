5037912630dcba5e7bea437bcadd51b998ea3f66
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-22 10:44:17 -0400

    add solution code

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ActiveRecord::Base
-  belongs_to :guest, :class_name => :User
   belongs_to :listing
-  has_many :reviews
-
+  belongs_to :guest, :class_name => "User"
+  has_one :review
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ActiveRecord::Base
-  belongs_to :guest, :class_name => :User
   belongs_to :reservation
+  belongs_to :guest, :class_name => "User"
 end
