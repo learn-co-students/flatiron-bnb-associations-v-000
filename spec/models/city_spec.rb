6a0d03bbb8d4a8880f4353a9f8d86c189de1f209
Steven Nunez <steven.nunez@gmail.com>
2016-01-13 15:12:55 -0500

    Update tests
    Previously tests were loaded in the rails_helper.
    Moving the setup code for each test and removing unnecessary setup will make
    the code easier to implement.

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe City do
-  it 'has a name' do 
-    expect(City.first.name).to eq('NYC')
+  before do
+    @new_york_city = City.create(name: 'NYC')
+
+    @financial_district = Neighborhood.create(name: 'Fi Di', city: @new_york_city)
+    @green_point = Neighborhood.create(name: 'Green Point', city: @new_york_city)
+    @brighton_beach = Neighborhood.create(name: 'Brighton Beach', city: @new_york_city)
   end
 
-  it 'has many neighborhoods' do 
-    expect(City.first.neighborhoods).to eq([@nabe1, @nabe2, @nabe3])
+  it 'has a name' do
+    expect(@new_york_city.name).to eq('NYC')
+  end
+
+  it 'has many neighborhoods' do
+    expect(@new_york_city.neighborhoods).to eq([@financial_district, @green_point, @brighton_beach])
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,20 +1,50 @@
 require 'rails_helper'
 
 describe Review do
+  before do
+    @new_york_city = City.create(name: 'NYC')
+
+    @financial_district = Neighborhood.create(name: 'Fi Di', city: @new_york_city)
+
+    @logan = User.create(name: "Logan")
+    @amanda = User.create(name: "Amanda")
+
+    @listing = Listing.create(
+        address: '123 Main Street',
+        listing_type: "private room",
+        title: "Beautiful Apartment on Main Street",
+        description: "My apartment is great. There's a bedroom. Close to subway",
+        price: 50.00,
+        neighborhood: @financial_district,
+        host: @amanda)
+
+    @reservation = Reservation.create(
+        checkin: '2014-04-25',
+        checkout: '2014-04-30',
+        listing: @listing,
+        guest: @logan)
+
+    @review = Review.create(
+        description: "This place was great!",
+        rating: 5,
+        guest: @logan,
+        reservation: @reservation)
+  end
+
   it 'has a description' do
-    expect(@review1.description).to eq("This place was great!") 
+    expect(@review.description).to eq("This place was great!")
   end
 
   it 'has a rating' do
-    expect(@review1.rating).to eq(5) 
+    expect(@review.rating).to eq(5)
   end
 
   it 'belongs to a guest' do
-    expect(@review1.guest).to eq(@logan) 
+    expect(@review.guest).to eq(@logan)
   end
 
   it 'belongs to a reservation' do
-    expect(@review1.reservation).to eq(@reservation1) 
+    expect(@review.reservation).to eq(@reservation)
   end
 
 end
