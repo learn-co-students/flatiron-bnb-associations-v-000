51047b6afe4b0a70c8eb5fc7a57c82154d6b89a0
mmmeyers <marsi286@gmail.com>
2016-02-05 10:54:21 -0500

    Done.

---
+++ db/migrate/20160204233842_add_cities.rb
@@ -0,0 +1,7 @@
+class AddCities < ActiveRecord::Migration
+  def change
+    create_table :cities do |t|
+      t.string :name
+    end
+  end
+end
---
+++ db/migrate/20160204233853_add_neighborhoods.rb
@@ -0,0 +1,8 @@
+class AddNeighborhoods < ActiveRecord::Migration
+  def change
+    create_table :neighborhoods do |t|
+      t.string :name
+      t.integer :city_id
+    end
+  end
+end
