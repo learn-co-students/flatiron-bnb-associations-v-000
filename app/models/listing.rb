cd8dd238e026dee30d45d794006f5eda667435ce
c1505 <coreym247@gmail.com>
2016-02-13 22:48:03 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   belongs_to :host, :class_name => "User"
   has_many :reviews, through: :reservations
+  has_many :guests, through: :reservations, :class_name => "User"
 end
