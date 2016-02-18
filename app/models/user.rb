51c02e8f8d7fba413158f415d49b199af99faa33
HarlemSquirrel <wittyman37@yahoo.com>
2016-02-18 15:00:23 -0500

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :listings, :foreign_key => 'host_id'
+  # has many trips as a guest
+  has_many :trips, foreign_key: 'guest_id', class_name: 'Reservation'
+  has_many :reservations, through: :listings, :foreign_key => 'host_id'
+  has_many :reviews, :foreign_key => 'guest_id'
+end
