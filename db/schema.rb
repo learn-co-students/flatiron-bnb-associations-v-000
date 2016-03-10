c7c421f249474f94e66078ed7fa3febe163a15fb
Alvinluipo <lualvin1234@gmail.com>
2016-03-10 17:49:31 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -20,26 +20,44 @@ ActiveRecord::Schema.define(version: 20160310202855) do
   end
 
   create_table "listings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "address"
+    t.string   "listing_type"
+    t.decimal  "price"
+    t.integer  "neighborhood_id"
+    t.integer  "host_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "neighborhoods", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reservations", force: :cascade do |t|
+    t.date     "checkin"
+    t.date     "checkout"
+    t.integer  "listing_id"
+    t.integer  "guest_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.integer  "rating"
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
