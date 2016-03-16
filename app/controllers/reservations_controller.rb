32470d89788f0e9cb2d14ba5ca226d391867bc56
Louiefigz <figueroa.aaron@gmail.com>
2016-03-16 16:52:28 -0400

    Done.

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,22 @@
+class ReservationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ app/models/reservation.rb
@@ -0,0 +1,9 @@
+class Reservation < ActiveRecord::Base
+  belongs_to :guest, :class_name => 'User'
+  belongs_to :listing
+  has_one :review
+
+
+
+
+end
