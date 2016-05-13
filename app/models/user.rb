b0f30d4471c7ce42a40937d399f0a6f73b86128e
McN33ly <rmcneely23@gmail.com>
2016-05-13 02:42:12 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many  :listings, :foreign_key => :host_id
   has_many  :reviews, :foreign_key => :guest_id
   has_many  :trips, class_name: "Reservation",  foreign_key: :guest_id
-  has_many  :reservations, :through => :listings, :source => :host
+  has_many  :reservations, :through => :listings
 end
