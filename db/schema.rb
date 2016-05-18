f7e3f93d76c84a75ce87a2edf499748c3f67b375
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-18 04:19:13 +0100

    Build Review Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518025236) do
+ActiveRecord::Schema.define(version: 20160518030805) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 20160518025236) do
     t.datetime "updated_at", null: false
     t.integer  "listing_id"
     t.integer  "guest_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rating"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
   end
 
   create_table "users", force: :cascade do |t|
