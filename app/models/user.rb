8cdb0afc8e13df32b434e8d6ceacac9225951fda
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-18 04:04:59 +0100

    Build User - Reservation Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :listings, foreign_key: 'host_id'
+  has_many :reservations, through: :listings
+  has_many :trips, class_name: 'Reservation', foreign_key: 'guest_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518021854) do
+ActiveRecord::Schema.define(version: 20160518025236) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20160518021854) do
     t.integer  "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "checkin"
+    t.datetime "checkout"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "listing_id"
+    t.integer  "guest_id"
   end
 
   create_table "users", force: :cascade do |t|
