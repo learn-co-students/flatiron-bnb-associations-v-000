8a06f90e97befa8f2ab5ebeaedf971e074f6705c
Nathaniel Miller <dukeoflaser@gmail.com>
2016-05-17 18:53:39 -0700

    Done.

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,4 +1,4 @@
 class Host < User
-  has_many :listings
-  has_many :reservations, through: :listings, :foreign_key => 'host_id'
+  # has_many :listings
+  has_many :reservations, through: :listings
 end
