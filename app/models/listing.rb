ad21ddbeeb28622472318b5fafad75fb65621e99
Christian Whitesides <whitesidescr@gmail.com>
2016-07-20 18:26:33 -0600

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,8 @@
 class Listing < ActiveRecord::Base
   belongs_to :host, class_name: "User"
   belongs_to :neighborhood
+
   has_many :reservations
-  has_many :guests, class_name: "User", through: :reservations
+  has_many :guests, through: :reservations, class_name: "User"
   has_many :reviews, through: :reservations
 end
