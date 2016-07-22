87a43865596a653dfb1079a8b560d9245a31aa3d
wedesignstudios <deelden@gmail.com>
2016-07-22 12:26:56 -0500

    refactored listing and user models to eliminate custom methods

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,7 @@
 class Listing < ActiveRecord::Base
-  belongs_to :host, :class_name => 'User'
+  belongs_to :host, class_name: 'User'
   belongs_to :neighborhood
   has_many :reservations
   has_many :reviews, through: :reservations
-
-  def guests    
-    Reservation.where(listing_id: self.id).map {|rsvp| rsvp.guest}
-  end
-
+  has_many :guests, through: :reservations, class_name: 'User'
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,5 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, through: :listings
   has_many :reviews, :foreign_key => 'guest_id'
-
-  def trips
-    Reservation.where(guest_id: self.id)
-  end
+  has_many :trips, class_name: 'Reservation', :foreign_key => 'guest_id'  
 end
