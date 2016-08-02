af1328a2588bbc8d5b0f230c10acd341c1d51797
kaileeagray <kaileeagray@gmail.com>
2016-08-02 14:05:55 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
-  has_many :reservations
+  has_many :reservations, through: :listings
   has_many :reviews, :foreign_key => 'guest_id'
 
 end
