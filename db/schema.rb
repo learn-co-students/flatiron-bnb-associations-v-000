aa09a030b46285f6b097f1e55ac19f2f11027cff
kaileeagray <kaileeagray@gmail.com>
2016-08-02 14:20:52 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802182605) do
+ActiveRecord::Schema.define(version: 20160802191708) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -59,6 +59,11 @@ ActiveRecord::Schema.define(version: 20160802182605) do
     t.integer "rating"
     t.integer "guest_id"
     t.integer "reservation_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
