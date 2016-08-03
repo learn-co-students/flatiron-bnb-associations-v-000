6aa6f23205b4fc322f98911620242b1d93197715
Jason <jasonkwong11@gmail.com>
2016-08-03 16:57:31 -0400

    done

---
+++ db/migrate/20160803205529_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer :guest_id
+      t.integer :listing_id
+      t.datetime :checkin
+      t.datetime :checkout
+      t.timestamps null: false
+    end
+  end
+end
