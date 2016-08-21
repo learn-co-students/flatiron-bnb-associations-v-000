6d53a527f46d85fc97b77c0002cb0368ac900adf
karuna24s <karuna24s@gmail.com>
2016-08-20 21:08:35 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
   belongs_to :host, :class_name => "User"
+  belongs_to :neighborhood
+  has_many :reviews, through: :reservations
+  has_many :guests, through: :reservations, :class_name => "User"
   has_many :reservations
 end
