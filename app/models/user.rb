cc1496f84e3e2d5ff2cfab59f6946c455668007a
miclp <michaelpeimani@gmail.com>
2016-09-02 16:40:04 -0700

    Refactor User has_many :reservations lines into one line

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ class User < ActiveRecord::Base
   # as host
   has_many :listings, :foreign_key => 'host_id'
 
+  # as host or guest
+  has_many :reservations, through: :listings
+
   # as guest
-  has_many :reservations, :foreign_key => 'guest_id'
   has_many :reviews, :foreign_key => 'guest_id'
-
-  # as host: moved this from line 4 to here to pass spec:
-  has_many :reservations, :foreign_key => 'host_id', through: :listings
-
   # trips for guests
   has_many :trips, :foreign_key => 'guest_id', class_name: "Reservation"
 end
