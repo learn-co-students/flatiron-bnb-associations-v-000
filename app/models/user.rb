2b37a3995026023f635b370cc01a9a850455d809
miclp <michaelpeimani@gmail.com>
2016-09-02 16:33:39 -0700

    Add #User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
+  # as host
   has_many :listings, :foreign_key => 'host_id'
-  # has_many :reservations, through: :listings
-  has_many :reservations, :foreign_key => 'guest_id'
 
-  has_many :reviews
+  # as guest
+  has_many :reservations, :foreign_key => 'guest_id'
+  has_many :reviews, :foreign_key => 'guest_id'
+
+  # as host: moved this from line 4 to here to pass spec:
+  has_many :reservations, :foreign_key => 'host_id', through: :listings
+
+  # trips for guests
+  has_many :trips, :foreign_key => 'guest_id', class_name: "Reservation"
 end
