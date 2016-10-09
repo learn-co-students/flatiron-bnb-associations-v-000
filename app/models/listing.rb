e193359d8d8cf1f0a81a5204cd7bec61f8c861e2
araf91 <araft50@yahoo.com>
2016-10-09 00:04:02 -0400

    Done.

---
+++ app/models/listing.rb
@@ -0,0 +1,9 @@
+class Listing < ActiveRecord::Base
+  belongs_to :host, :class_name => "User"
+  belongs_to :city
+  belongs_to :neighborhood
+  has_many :reservations
+  has_many :guests, through: :reservations
+  has_many :reviews, through: :reservations
+
+end
---
+++ db/migrate/20160719000139_change_checkin_to_reservations.rb
@@ -0,0 +1,6 @@
+class ChangeCheckinToReservations < ActiveRecord::Migration
+  def change
+    change_column :reservations, :checkin, :datetime
+    change_column :reservations, :checkout, :datetime
+  end
+end
