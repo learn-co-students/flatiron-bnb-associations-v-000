0938fc2e694b533b8d0e567459bf4e30543a386b
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-11 22:19:26 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, through: :listings
+
+  def trips
+    Reservation.all
+  end
+
+  def reviews
+    Review.all
+  end
 end
