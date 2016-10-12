82bdea5a0a2e5b61dce053d2b87d0be67ef2bfb7
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-11 21:37:59 -0400

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,3 @@
 class Reservation < ActiveRecord::Base
+  belongs_to :listing
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012012806) do
+ActiveRecord::Schema.define(version: 20161012013715) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20161012012806) do
     t.string   "guest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "listing_id"
   end
 
   create_table "reviews", force: :cascade do |t|
