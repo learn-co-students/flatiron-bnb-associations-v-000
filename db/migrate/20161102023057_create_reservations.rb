0479d3cd39802d2b6f8862559522d623ec15197c
bryan <bryan@snowboardtechie.com>
2016-11-02 08:51:42 -0600

    saving work

---
+++ db/migrate/20161102023057_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.string :checkin
+      t.string :checkout
+      t.integer :listing_id
+      t.integer :guest_id
+      t.timestamps null: false
+    end
+  end
+end
