6bbab2b6b5e07ef4103f8755665d77b94e65d5a8
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-25 22:33:34 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :listings, :foreign_key => 'host_id'
+  #has_many :reservations, :foreign_key => 'guest_id'
+  has_many :reviews, :foreign_key => 'guest_id'
+  has_many :reservations, through: :listings
+
+
+
 end
