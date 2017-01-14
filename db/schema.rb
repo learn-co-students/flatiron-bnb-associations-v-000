b063ddf1a772a4f0b99deed2a75be5308bada4a4
Scott Savage <snsavage@gmail.com>
2017-01-13 19:46:07 -0500

    Add User resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113211403) do
+ActiveRecord::Schema.define(version: 20170114004357) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20170113211403) do
   create_table "neighborhoods", force: :cascade do |t|
     t.string   "name"
     t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
