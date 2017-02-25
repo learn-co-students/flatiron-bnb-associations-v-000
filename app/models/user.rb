d483dbc4eb08a6597bc094349c72a97277cfc5aa
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-02-25 23:32:27 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :listings, :foreign_key => 'host_id'
+  has_many :reservations, through: :listings, :foreign_key => 'host_id'
+  has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
+  has_many :reviews, :foreign_key => 'guest_id'
 end
