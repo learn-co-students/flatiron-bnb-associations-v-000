c41dda7c12c1aeff47041e66ad513741706fc78c
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-07 02:44:51 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :name 
   
   has_many :listings, :foreign_key => "host_id"
-  has_many :reservations, through: :listings, source: "host_id", :as => :host   
-  # has_many :reservations, :foreign_key => "guest_id", :as => :guest
+  has_many :reservations, through: :listings, :foreign_key => "host_id"
+  has_many :reviews, :foreign_key => "guest_id"
+  has_many :trips, :class_name => "Reservation", :foreign_key => "guest_id"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307014900) do
+ActiveRecord::Schema.define(version: 20170307020749) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 20170307014900) do
     t.integer  "listing_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rating"
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
