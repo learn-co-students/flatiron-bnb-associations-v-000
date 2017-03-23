3dc6d4971b39a359c7b61bd46e181bf6bb0e6d27
Callie Brazil <cmabrazil@gmail.com>
2017-03-23 20:48:44 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323201650) do
+ActiveRecord::Schema.define(version: 20170323204036) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name",       null: false
@@ -34,6 +34,30 @@ ActiveRecord::Schema.define(version: 20170323201650) do
   create_table "neighborhoods", force: :cascade do |t|
     t.string   "name"
     t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "checkin"
+    t.date     "checkout"
+    t.integer  "guest_id"
+    t.integer  "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rating"
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
