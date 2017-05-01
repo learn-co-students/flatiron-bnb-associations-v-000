9f5487929fcc2fc18d4efd934a02a9da35c0a733
anytimesoon <damien@delartigue.co.uk>
2017-05-01 12:19:01 +0100

    Done.

--- db/migrate/20170430220604_create_users.rb
+++ db/migrate/20170430220604_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.integer :host_id
+      t.integer :guest_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430220604) do
+ActiveRecord::Schema.define(version: 20170501100346) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -38,9 +38,28 @@ ActiveRecord::Schema.define(version: 20170430220604) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date     "checkin"
+    t.date     "checkout"
+    t.integer  "guest_id"
+    t.integer  "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rating"
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "host_id"
+    t.integer  "guest_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
