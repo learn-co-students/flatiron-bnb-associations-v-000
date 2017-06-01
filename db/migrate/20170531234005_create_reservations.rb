bdae5799b949a4c9eabb3169b208412d85341d68
github username apalski <github email address annette.drapalski@gmail.com>
2017-06-01 12:59:36 +1000

    Done.

---
+++ db/migrate/20170531234005_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+    	t.date :checkin
+    	t.date :checkout
+    	t.integer :listing_id
+      t.integer :guest_id
+    	t.integer :review_id
+
+      t.timestamps null: false
+    end
+  end
+end
