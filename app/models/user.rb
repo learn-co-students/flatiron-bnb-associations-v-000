38e0355f180e72ceb51cdcfcfd30f513a39a3f3d
essbee808 <sheena.myb@gmail.com>
2020-12-21 20:58:45 -0800

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
    #Host
    has_many :listings, :foreign_key => 'host_id'
    has_many :reservations, :foreign_key => 'host_id'
+
    #Guest
    has_many :reservations, :foreign_key => 'guest_id'
    has_many :reviews, :foreign_key => 'guest_id'
-   has_many :trips
+ 
 end
