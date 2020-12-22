5c308e034ec25d96deaba609c1f22c5f4868b2ac
essbee808 <sheena.myb@gmail.com>
2020-12-21 17:55:05 -0800

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
     belongs_to :host, :class_name => "User"
     belongs_to :neighborhood
+    
     has_many :reservations
     has_many :reviews, through: :reservations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201222013852) do
+ActiveRecord::Schema.define(version: 20201222014410) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20201222013852) do
     t.string   "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "city_id"
   end
 
   create_table "reservations", force: :cascade do |t|
